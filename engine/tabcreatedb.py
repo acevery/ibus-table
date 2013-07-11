84a971c8144c2a0be13f810474cee40d5816c676
Yuwei Yu <acevery@gmail.com>
2013-07-11 13:14:42 +0800

    update copyright info

--- engine/tabcreatedb.py
+++ engine/tabcreatedb.py
@@ -4,7 +4,7 @@
 #
 # ibus-table - The Tables engine for IBus
 #
-# Copyright (c) 2008-2009 Yu Yuwei <acevery@gmail.com>
+# Copyright (c) 2008-2013 Yuwei Yu <acevery@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
